e4a62e5089e252c66f710105cb053308902aab8f
Sean Rose <sean1rose@gmail.com>
2014-01-11 12:50:12 -0800

    bevel corner

--- script.js
+++ script.js
@@ -447,7 +447,7 @@ $(document).ready(function () {
         display();
     }); 
 
-    $(consoleOuter).corner("bite");
+    $(consoleOuter).corner("bevel");
     $(console).corner();
     $(txtBox).corner();
     $(subBtn).corner();
